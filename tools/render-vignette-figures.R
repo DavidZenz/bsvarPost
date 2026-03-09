3d75ed8ae2f17f273a26ec4de144bc92c554ba06
David Zenz <office@davidzenz.com>
2026-03-09 16:35:00 +0100

    Add pre-rendered vignette figures

---
+++ tools/render-vignette-figures.R
@@ -0,0 +1,87 @@
+dir.create("vignettes/figures", recursive = TRUE, showWarnings = FALSE)
+
+.save_gg <- function(path, plot, width = 8, height = 5, dpi = 150) {
+  ggplot2::ggsave(
+    filename = path,
+    plot = plot,
+    width = width,
+    height = height,
+    dpi = dpi
+  )
+}
+
+.save_base <- function(path, expr, width = 1600, height = 1200, res = 150) {
+  grDevices::png(path, width = width, height = height, res = res)
+  on.exit(grDevices::dev.off(), add = TRUE)
+  force(expr)
+}
+
+library(bsvars)
+library(bsvarSIGNs)
+library(bsvarPost)
+library(ggplot2)
+
+data(us_fiscal_lsuw)
+set.seed(101)
+spec_bsvar <- specify_bsvar$new(us_fiscal_lsuw, p = 1)
+post_bsvar <- estimate(spec_bsvar, S = 100, thin = 1, show_progress = FALSE)
+spec_bsvar_alt <- specify_bsvar$new(us_fiscal_lsuw, p = 3)
+post_bsvar_alt <- estimate(spec_bsvar_alt, S = 100, thin = 1, show_progress = FALSE)
+
+cdm_bsvar <- cdm(post_bsvar, horizon = 8)
+.save_base(
+  "vignettes/figures/cdm-showcase.png",
+  plot(cdm_bsvar)
+)
+
+cmp_irf <- compare_irf(p1 = post_bsvar, p2 = post_bsvar_alt, horizon = 8)
+cmp_irf_focus <- cmp_irf[
+  cmp_irf$variable == unique(cmp_irf$variable)[1] &
+    cmp_irf$shock == unique(cmp_irf$shock)[1],
+  ,
+  drop = FALSE
+]
+.save_gg(
+  "vignettes/figures/compare-irf-showcase.png",
+  template_bsvar_plot(
+    ggplot2::autoplot(cmp_irf_focus),
+    family = "comparison",
+    preset = "paper"
+  ),
+  width = 8,
+  height = 4.5
+)
+
+rep_bsvar <- median_target_irf(post_bsvar, horizon = 8)
+.save_gg(
+  "vignettes/figures/representative-showcase.png",
+  publish_bsvar_plot(rep_bsvar, preset = "paper"),
+  width = 8,
+  height = 5
+)
+
+data(optimism)
+sign_irf <- matrix(c(0, 1, rep(NA, 23)), 5, 5)
+set.seed(202)
+spec_sign <- specify_bsvarSIGN$new(
+  optimism * 100,
+  p = 4,
+  sign_irf = sign_irf
+)
+post_sign <- estimate(spec_sign, S = 100, thin = 1, show_progress = FALSE)
+
+diag_sign <- acceptance_diagnostics(post_sign)
+.save_gg(
+  "vignettes/figures/diagnostics-showcase.png",
+  publish_bsvar_plot(diag_sign, preset = "paper"),
+  width = 8,
+  height = 4.5
+)
+
+hd_sign <- plot_hd_event(post_sign, start = 1, end = 4)
+.save_gg(
+  "vignettes/figures/hd-event-showcase.png",
+  hd_sign,
+  width = 8,
+  height = 5
+)
